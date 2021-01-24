17bf891f3e539ce43afdc7de16652e83527094f6
Guillermo-Tobon <memot18@hotmail.com>
2021-01-24 00:12:40 -0500

    Ajuste en tickets

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -159,6 +159,49 @@ const customInitFunctions = () =>{
   
   });
 
+  //**************************************************************************/
+  //*********           <!-- jQuery file upload -->            ***************/
+  //**************************************************************************/
+
+  // Basic
+  $('.dropify').dropify();
+
+  // Translated
+  $('.dropify-fr').dropify({
+      messages: {
+        default: 'Arrastre y suelte un archivo aquí o haga clic aquí',
+        replace: 'Arrastra y suelta o haz clic para reemplazar',
+        remove:  'Remover',
+        error:   'Vaya, se agregó algo incorrecto.'
+      }
+  });
+
+  // Used events
+  var drEvent = $('#input-file-events').dropify();
+
+  drEvent.on('dropify.beforeClear', function(event, element) {
+      return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+  });
+
+  drEvent.on('dropify.afterClear', function(event, element) {
+      alert('File deleted');
+  });
+
+  drEvent.on('dropify.errors', function(event, element) {
+      console.log('Has Errors');
+  });
+
+  var drDestroy = $('#input-file-to-destroy').dropify();
+  drDestroy = drDestroy.data('dropify')
+  $('#toggleDropify').on('click', function(e) {
+      e.preventDefault();
+      if (drDestroy.isDropified()) {
+          drDestroy.destroy();
+      } else {
+          drDestroy.init();
+      }
+  })
+
 }
 
 customInitFunctions();
